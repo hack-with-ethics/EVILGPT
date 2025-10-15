a7fd6d9c3fdee7b8625c6b33f4d73056e508c2d2
sanjai sa🥷🔒[ Hack-with-ethics] <111492120+hack-with-ethics@users.noreply.github.com>
2025-10-15 20:42:09 +0530

    Add files via upload

---
+++ gptweb.js
@@ -0,0 +1,49 @@
+async function askGPT(params) {
+  const response = await fetch("https://router.huggingface.co/v1/chat/completions", {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer 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",  // ❌ DANGER
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      model: "deepseek-ai/DeepSeek-V3-0324",
+      messages: [
+        { role: "system", content: "You are a cybersecurity teacher" },
+        { role: "user", content: params }
+      ]
+    })
+  });
+  const data = await response.json();
+  alert("responded")
+  return data.choices[0].message.content;
+  
+}
+
+
+async function RunGpt() {
+    var inputval = document.getElementById("inp")
+    let value = inputval.value
+    inputval.value = ""
+    var response = await askGPT(value.trim())
+    
+    console.log(response)
+    var contentWriter= ""
+    response = response.split(".");
+    let para = document.getElementById("disp");
+      if (!para) {
+        para = document.createElement("p");
+        para.id = "disp";
+        document.body.appendChild(para);
+      }
+    
+    for (let i = 0; i < response.length; i++) {
+        let sentence = response[i].trim();
+        if (sentence) {
+          contentWriter += sentence + ".<br><br>";
+          para.innerHTML = contentWriter;
+
+          // wait 400ms between lines
+          await new Promise(resolve => setTimeout(resolve, 400));
+        }
+      }
+}
